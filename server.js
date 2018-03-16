ae40d528228bc221b4a40ddb0b744e73396d1440
Manjilan <nakarmimanjila@yahoo.com>
2018-03-16 12:13:44 -0700

    add routes for cards

--- server.js
+++ server.js
@@ -14,8 +14,37 @@ app.use(function(req, res, next) {
  next();
 });
 
+//----------------Routes------------->
+//View of all cards
+app.get("/api/cards", function(req, res){
+  Card.find({}, function(err, allCards){
+    if (err){
+      console.log(err);
+    } else {
+      res.render("cards", {cards: allCards, error: null});
+    }
+});
+});
+
+//Showpage for individual cards
+app.get("/api/cards/:id", function(req, res){
+  Card.findOne({_id: CardId}, function(err, foundCard){
+    console.log(foundCard);
+    if (err){
+      console.log(err);
+    } else{
+      foundCard.title = req.body.title || foundCard.title;
+      foundCard.description = req.body.description || foundCard.description;
+      foundCard.image = req.body.title || foundCard.image;
+    }
+  })
+});
 
 
+
+
+
+//-------------Server------------->
 app.listen(process.env.PORT || 3000, function(){
 
   console.log("listening..");
