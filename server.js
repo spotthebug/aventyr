36b520f94b4af2464474bdf658f4033561417b99
Manjilan <nakarmimanjila@yahoo.com>
2018-03-16 10:36:29 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+var bodyParser = require('body-parser');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var db = require('./models');
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log(listening...);
+});
